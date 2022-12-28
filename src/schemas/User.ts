99711628ba46543573f79a892634d72ad7166300
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-12-28 09:43:49 -0300

    Update User.ts

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -1,20 +1,60 @@
-import { Document, Schema, Model, model } from 'mongoose'
-import { UserInterface } from '../interfaces/user'
+import { Schema, model, Document, Model, DocumentQuery } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from '../config';
+import { UsuarioInterface } from '../interfaces/User';
 
-export interface UserModel extends UserInterface, Document {
-  fullName(): string
+interface UsuarioModel extends UsuarioInterface, Document {
+    compareHash(data: string): Promise<boolean>;
+    generateToken(): string;
 }
 
-const UserSchema = new Schema({
-  email: String,
-  firstName: String,
-  lastName: String
-}, {
-  timestamps: true
-})
-
-UserSchema.methods.fullName = function (): string {
-  return (this.firstName.trim() + ' ' + this.lastName.trim())
+interface UsuarioStatic extends Model<UsuarioModel> {
+    buscaTodosChat(idUsuario: string): DocumentQuery<UsuarioModel[], UsuarioModel>;
 }
 
-export const User: Model<UserModel> = model<UserModel>('User', UserSchema)
+const UsuarioSchema = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    senha: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: false
+    }
+});
+
+UsuarioSchema.pre<UsuarioModel>('save', async function hashPassword(next) {
+    if (!this.isModified('senha')) next();
+
+    this.senha = await bcrypt.hash(this.senha, 8);
+});
+
+UsuarioSchema.pre<UsuarioModel>('save', function createAvatar() {
+    const randomId = Math.floor(Math.random() * (1000000)) + 1;
+
+    this.avatar = `https://api.adorable.io/avatars/285/${randomId}.png`;
+});
+
+UsuarioSchema.methods.compareHash = function(hash: string): Promise<boolean> {
+    return bcrypt.compare(hash, this.senha);
+}
+
+UsuarioSchema.methods.generateToken = function(): string {
+
+    const tokenDecoded = { _id: String(this._id), nome: this.nome, avatar: this.avatar };
+
+    return jwt.sign(tokenDecoded, config.SALT_KEY, {
+        expiresIn: '1d'
+    });
+}
+
+UsuarioSchema.statics.buscaTodosChat = function(idUsuario: string): DocumentQuery<UsuarioModel[], UsuarioModel> {
+    return this.find({ _id: { $ne: idUsuario } });
+}
+
+export default model<UsuarioModel, UsuarioStatic>('Usuario', UsuarioSchema);
